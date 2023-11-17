a54fdf9576029580843f528e8a6ac93379a3119e
Nurislam <raiymbeko0332@gmail.com>
2023-11-17 16:03:55 +0600

    First Comm

---
+++ client/src/pages/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "./../components/Layout/Layout";
+
+const About = () => {
+  return (
+    <Layout title={"About us"}>
+      <div className="row contactus ">
+        About Page
+      </div>
+    </Layout>
+  );
+};
+
+export default About;
